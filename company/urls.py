b834fae9454a7e4f62ac5e5bf4e8756b992f88a0
Sivakumar3001 <sivakumar.tk@mitrahsoft.com>
2025-04-02 20:47:11 +0530

    moved endpoints to api.py

--- company/urls.py
+++ company/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
+from . import api
 
 urlpatterns = [
-    path("", views.company_list, name="company_list"),
-    path('create',views.company_create,name='company_create'),
-    path('update/<int:pk>',views.company_update, name="company_update"),
-    path('delete/<int:pk>',views.company_delete,name='company_delete')
+    path("", api.company_list, name="company_list"),
+    path('create',api.company_create,name='company_create'),
+    path('update/<int:pk>',api.company_update, name="company_update"),
+    path('delete/<int:pk>',api.company_delete,name='company_delete')
 ]
