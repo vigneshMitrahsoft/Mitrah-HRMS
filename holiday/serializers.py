3708316f18a88466ada7facc6cf7b348e8783287
abisheikk <abisheik.k@mitrahsoft.com>
2025-05-06 20:47:22 +0530

    refactor: update model references from Holiday to holiday for consistency and removed the update function

--- holiday/serializers.py
+++ holiday/serializers.py
@@ -1,9 +1,9 @@
-from .models import Holiday
+from .models import holiday
 from rest_framework import serializers
  
 class HolidaySerializer(serializers.ModelSerializer):
     class Meta:
-        model = Holiday
+        model = holiday
         fields = '__all__'
         read_only_fields = ['holiday_id', 'created_at', 'updated_at']
     
@@ -13,7 +13,7 @@ class HolidayCreateSerializer(serializers.Serializer):
     holiday_date = serializers.DateField(required=True)
 
     def validate(self,data):
-        if Holiday.objects.filter(holiday_date = data['holiday_date']).exists():
+        if holiday.objects.filter(holiday_date = data['holiday_date']).exists():
             raise serializers.ValidationError("Holiday date already exists")
         return data
 
@@ -29,9 +29,3 @@ class HolidayUpdateSerializer(serializers.Serializer):
     #             raise serializers.ValidationError("Holiday date must be a string")
     #     return data
 
-    def update(self, instance, validated_data):
-        instance.occasion = validated_data.get('occasion', instance.occasion)
-        instance.leave_type = validated_data.get('leave_type', instance.leave_type)
-        instance.holiday_date = validated_data.get('holiday_date', instance.holiday_date)
-        instance.save()
-        return instance
