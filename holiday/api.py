8f8f8967f8667fec2171712fae440c897aa2bd2e
abisheikk <abisheik.k@mitrahsoft.com>
2025-05-06 20:46:13 +0530

    refactor: update model references from Holiday to holiday for consistency moved update_holiday from serializers.pt to api.py

--- holiday/api.py
+++ holiday/api.py
@@ -5,7 +5,7 @@ from .serializers import *
 from rest_framework.decorators import api_view
 import base64
 import io
-from .models import Holiday
+from .models import holiday
 import pandas as pd
 
 
@@ -48,7 +48,7 @@ def index(request):
                 occasion = str(row['occasion']).strip()
                 leave_type = str(row['leave_type']).strip()
                 #, is_created
-                holiday_obj, is_created = Holiday.objects.update_or_create(                                                                
+                holiday_obj, is_created = holiday.objects.update_or_create(
                         holiday_date=date,
                         defaults={
                                 'occasion': occasion,
@@ -77,13 +77,13 @@ def index(request):
         )
         return Response({"status": "error", "message": error_message}, status=500)
 
-@api_view(['POSt'])
+@api_view(['POST'])
 def create_holiday(request):
     try:
-        serializer = HolidayCreateSerializer(data = request.data)        
-        if serializer.is_valid():        
+        serializer = HolidayCreateSerializer(data = request.data)
+        if serializer.is_valid():
             data = serializer.validated_data            
-            create_holiday = Holiday.objects.create(**data)
+            holiday.objects.create(**data)
             return Response({"statuscode":status.HTTP_201_CREATED,"status":"success",'message':f'{serializer.data['occasion']} Holiday stored'},status=status.HTTP_201_CREATED)
         
     except Exception as e:
@@ -99,33 +99,26 @@ def create_holiday(request):
 @api_view(['PATCH'])
 def update_holiday(request,id):
     try:
-        holiday_obj = Holiday.objects.get(holiday_id = id)
-    except Holiday.DoesNotExist:
+        holiday_obj = holiday.objects.get(holiday_id = id)
+    except holiday.DoesNotExist:
         return Response({"status": "error", "message": "Holiday not found"}, status=404)
-    except Exception as e:
-        error_message = (
-            type(e).__name__,          # TypeError
-            __file__,                  # /tmp/example.py
-            e.__traceback__.tb_lineno,  # line number
-            str(e)
-        )
-        return Response({"status": "error", "message": error_message}, status=500)
+    
     serializer = HolidayUpdateSerializer(holiday_obj,data = request.data, partial = True)
+    
     if serializer.is_valid():
-        serializer.save()
+        print('inside serializer')
+        holiday.objects.filter(holiday_id = id).update(**serializer.validated_data)        
+        print(serializer)
         return Response({"statuscode":status.HTTP_200_OK,"status": "success", "message": "Holiday updated successfully","data" : serializer.data}, status=200)
     return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
 
-    
 @api_view(['POST'])    
 def delete_holiday(request, id):
     try:
-        holiday_obj = Holiday.objects.get(holiday_id=id)
+        holiday_obj = holiday.objects.get(holiday_id=id)
         holiday_obj.delete()
         return Response({"status": "success", "message": "Holiday deleted successfully"}, status=200)
-    except Holiday.DoesNotExist:
+    except holiday.DoesNotExist:
         return Response({"status": "error", "message": "Holiday not found"}, status=404)
     except Exception as e:
         return Response({"status": "error", "message": str(e)}, status=500)
-    
-        
