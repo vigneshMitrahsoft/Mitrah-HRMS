b71d9aaaf27515611f93c018dba35588cb0c1568
NaveenG0107 <naveenkumar.g@mitrahsoft.com>
2025-05-09 11:42:50 +0530

    add company paycycle day

--- company/models.py
+++ company/models.py
@@ -30,6 +30,7 @@ class company_Settings(models.Model):
     sick_leaves  = models.FloatField(blank=True, null=True)
     casual_leaves = models.FloatField(blank=True, null=True)
     permission_hours = models.TimeField(blank=True, null=True)
+    pay_cycle_day = models.IntegerField(blank = True, null = True)
 
     class Meta:
         db_table = 'company_settings' 
--- company/serializers.py
+++ company/serializers.py
@@ -4,40 +4,60 @@ from .models import company
 
 class companySerializer(serializers.ModelSerializer):
 
-    class Meta:
-        model = company
-        fields = '__all__'
+	class Meta:
+		model = company
+		fields = '__all__'
 
 class companyCreateSerializer(serializers.Serializer):
-    company_name = serializers.CharField()
-    address =serializers.CharField()
-    hra = serializers.FloatField()
-    employer_ESI = serializers.FloatField()
-    employee_ESI = serializers.FloatField()
-    employer_PF = serializers.FloatField()
-    employee_PF = serializers.FloatField()
-    leave_compensation = serializers.FloatField()
-    basic_work_hours = serializers.FloatField()
-    sick_leaves = serializers.FloatField()
-    casual_leaves = serializers.FloatField()
-    basic_pay = serializers.FloatField()
-    other_allowances = serializers.FloatField()
-    permission_hours = serializers.TimeField()
+	company_name = serializers.CharField()
+	address =serializers.CharField()
+	hra = serializers.FloatField()
+	employer_ESI = serializers.FloatField()
+	employee_ESI = serializers.FloatField()
+	employer_PF = serializers.FloatField()
+	employee_PF = serializers.FloatField()
+	leave_compensation = serializers.FloatField()
+	basic_work_hours = serializers.FloatField()
+	sick_leaves = serializers.FloatField()
+	casual_leaves = serializers.FloatField()
+	basic_pay = serializers.FloatField()
+	other_allowances = serializers.FloatField()
+	permission_hours = serializers.TimeField()
+
+	def validate(self,data):
+		errors = []
+
+		if data['basic_pay'] == 100:
+			data['hra'] = 0
+			data['other_allowances'] = 0
+		if not (0 <= data['basic_pay'] <= 100):
+			errors.append("Basic pay percent must be between 0 and 1.")
+		if not (0 <= data['hra'] <= 100):
+			errors.append("HRA percent must be between 0 and 1.")
+		if not (0 <= data['other_allowances'] <= 100):
+			errors.append("Other allowance percent must be between 0 and 1.")
+		if data['basic_pay'] + data['hra'] + data['other_allowances'] > 100:
+			errors.append("Total salary component percentages should not exceed 100%.")
+		
+		if errors:
+			raise serializers.ValidationError(errors)
+	
+		return data
 
 
 class companyUpdateSerializer(serializers.Serializer):
-    company_name = serializers.CharField()
-    address =serializers.CharField()
-    hra = serializers.FloatField()
-    employer_ESI = serializers.FloatField()
-    employee_ESI = serializers.FloatField()
-    employer_PF = serializers.FloatField()
-    employee_PF = serializers.FloatField()
-    leave_compensation = serializers.FloatField()
-    basic_work_hours = serializers.FloatField()
-    sick_leaves = serializers.FloatField()
-    casual_leaves = serializers.FloatField()
-    basic_pay = serializers.FloatField()
-    other_allowances = serializers.FloatField()
-    permission_hours = serializers.TimeField()
+	company_name = serializers.CharField()
+	address =serializers.CharField()
+	hra = serializers.FloatField()
+	employer_ESI = serializers.FloatField()
+	employee_ESI = serializers.FloatField()
+	employer_PF = serializers.FloatField()
+	employee_PF = serializers.FloatField()
+	leave_compensation = serializers.FloatField()
+	basic_work_hours = serializers.FloatField()
+	sick_leaves = serializers.FloatField()
+	casual_leaves = serializers.FloatField()
+	basic_pay = serializers.FloatField()
+	other_allowances = serializers.FloatField()
+	permission_hours = serializers.TimeField()
 
