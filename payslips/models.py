517c19f0e1f7c1cc6b9cbafea6f744d33e579e14
Sivakumar3001 <sivakumar.tk@mitrahsoft.com>
2025-05-12 20:40:57 +0530

    payslip and loan alteration

--- payslips/models.py
+++ payslips/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from employee.models import employee
+
 
 # Create your models here.
+
+class Payslip(models.Model):
+	payslip_id = models.AutoField(primary_key=True)
+	employee = models.ForeignKey(employee, on_delete=models.CASCADE)
+	month = models.CharField(max_length=20)
+	year = models.IntegerField(blank=True, null=True)
+	basic_pay = models.FloatField(blank=True, null=True)
+	hra = models.FloatField(blank=True, null=True)
+	other_allowances = models.FloatField(blank=True, null=True)
+	travel_allowance = models.FloatField(blank=True, null=True)
+	employee_pf = models.FloatField(blank=True, null=True)
+	employee_esi = models.FloatField(blank=True, null=True)
+	employer_pf = models.FloatField(blank=True, null=True)
+	employer_esi = models.FloatField(blank=True, null=True)
+	loan_emi = models.FloatField(blank=True, null=True)
+	lop = models.FloatField(blank=True, null=True)
+	net_pay = models.FloatField(blank=True, null=True)
+	created_at = models.DateTimeField(auto_now_add=True)
+	updated_at = models.DateTimeField(auto_now=True)
+	updated_by = models.IntegerField(blank=True, null=True)
+	status = models.CharField(max_length=20, default='pending')
+	class Meta:
+		db_table = 'payslips'
--- payslips/urls.py
+++ payslips/urls.py
@@ -3,5 +3,8 @@ from . import api
 
 
 urlpatterns = [
-	path('<int:id>',api.generate_payslip,name='get_payslips')
+	path('<int:id>',api.get_payslip,name='get_payslip'),
+	path('',api.get_all_payslips,name='get_all_payslips'),
+	path('generate',api.generate_all_payslips,name='generate_all_payslips'),
+	path('update/<int:id>',api.update_payslip_status,name='update_payslip_status'),
 ]
