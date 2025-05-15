7bd4f9fa1c937eef5b7847ab424963122ddd6059
balaji362 <balajistr27@gmail.com>
2025-05-15 20:08:06 +0530

    Completed

--- main/settings.py
+++ main/settings.py
@@ -182,3 +182,10 @@ SIMPLE_JWT = {
 }
 
 from .settings_local import *
+
+PASSWORD_HASHERS=[
+        'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+        'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+        'django.contrib.auth.hashers.Argon2PasswordHasher',
+        'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
+    ]
--- singleton.py
+++ singleton.py
@@ -3,6 +3,18 @@ import psycopg2
 from datetime import datetime
 from django.contrib.auth.hashers import make_password
 
+import os
+import django
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'main.settings')  # 🔁 Change this to your actual settings module path
+django.setup()
+
+from django.conf import settings
+
+# Now you can use make_password
+plain_password = 'hr@rtesting'
+hashed_password = make_password(plain_password)
+
 data = settings.DATABASES['default']
 conn = psycopg2.connect(
 	host = data['HOST'],
@@ -12,11 +24,7 @@ conn = psycopg2.connect(
 	password = data['PASSWORD']
 	
 )
-# plain_password = "balaji@123"
-# if plain_password:
-# 	hashed_password = make_password(plain_password)   
-# 	plain_password = hashed_password 
-# print("plain------>",plain_password)
+
 cursor = conn.cursor()
 cursor.execute(f"INSERT INTO company(company_name, address, created_at, updated_at, updated_by,is_active)VALUES('testing','testing','{datetime.now()}','2025-05-06 22:16:05.786801+05:30',1,True)RETURNING company_id")
 company_id = cursor.fetchone()[0]
@@ -24,7 +32,18 @@ cursor.execute(f"INSERT INTO company_settings(hra, employer_esi, employee_esi, e
 cursor.execute("INSERT INTO roles(role_name)VALUES('Admin'),('HR'),('HR Admin'),('Staff')")
 cursor.execute("INSERT INTO employee_type(type_name)VALUES('Full Time'),('Part Time'),('Contract') RETURNING type_id")
 type_id = cursor.fetchone()[0]
-cursor.execute(f"INSERT INTO employee(company_id, first_name, last_name, email, password, date_of_birth, address, date_of_joining, type_id, created_at, updated_at, created_by, updated_by, is_active, is_superuser)VALUES({company_id},'hr','testing','hr.bala@gmail.com','hr@rtesting', '1998-01-01','testing','2024-01-01',{type_id},'{datetime.now()}','2025-05-06 22:16:05.786801+05:30',1,1,true,false)")
+cursor.execute(f"INSERT INTO employee(company_id, first_name, last_name, email, password, date_of_birth, address, date_of_joining, type_id, created_at, updated_at, created_by, updated_by, is_active, is_superuser)VALUES({company_id},'hr','testing','hr.bala3@gmail.com','{hashed_password}', '1998-01-01','testing','2024-01-01',{type_id},'{datetime.now()}','2025-05-06 22:16:05.786801+05:30',1,1,true,false)")
+cursor.execute("SELECT employee_id FROM employee WHERE email = 'hr.bala3@gmail.com'")
+employee_id = cursor.fetchone()[0]
+
+role_ids = [5,6]
+for role_id in role_ids:
+	cursor.execute(f"""
+		INSERT INTO employee_roles (employee_id, role_id, is_active, created_at, updated_at, created_by, updated_by)
+		VALUES ({employee_id}, {role_id}, TRUE, '{datetime.now()}', '{datetime.now()}', 1, 1)
+	""")
+
+
 conn.commit()
 cursor.close()
 conn.close()
