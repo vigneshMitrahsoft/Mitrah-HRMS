e4516e9d0bcd9238849eee0e1909b595ac650881
Sivakumar3001 <sivakumar.tk@mitrahsoft.com>
2025-05-23 12:50:34 +0530

    added patch for tax slab

--- taxdeduction/api.py
+++ taxdeduction/api.py
@@ -3,7 +3,7 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework import status
 from rest_framework.exceptions import APIException
-from taxdeduction.serializer import TaxSlabSerializer, UpdateTaxregimeSerializer
+from taxdeduction.serializer import TaxSlabSerializer, TaxSlabUpdateSerializer, UpdateTaxregimeSerializer
 from .models import financial_year, tax_regimes, tax_slabs, employee_tax_regimes
 from datetime import date
 from decimal import Decimal
@@ -25,10 +25,10 @@ def setup_financial_year_and_regimes(request):
 
 		# Check if the financial year already exists
 		fy, created = financial_year.objects.get_or_create(
-			start_date=fy_start,
-			end_date=fy_end,
-			is_active=True,
-			defaults={'year_label': fy_label}
+			start_date = fy_start,
+			end_date = fy_end,
+			is_active = True,
+			defaults = {'year_label': fy_label}
 		)
 		if created:
 			print("Financial year created:", fy)
@@ -37,7 +37,7 @@ def setup_financial_year_and_regimes(request):
 		
 		# Check if the tax regimes already exist
 		old_regime, created = tax_regimes.objects.get_or_create(
-			regime_name='Old Regime'
+			regime_name = 'Old Regime'
 		)
 		if created:
 			print("Old regime created:", old_regime)
@@ -45,7 +45,7 @@ def setup_financial_year_and_regimes(request):
 			print("Old regime already exists:", old_regime)
 
 		new_regime, created = tax_regimes.objects.get_or_create(
-			regime_name='New Regime'
+			regime_name = 'New Regime'
 		)
 		if created:
 			print("New regime created:", new_regime)
@@ -74,67 +74,85 @@ def setup_tax_slabs(request):
 		fy_end = date(next_year, 3, 31)
 
 		try:
-			fy = financial_year.objects.get(start_date=fy_start, end_date=fy_end, is_active=True)
+			fy = financial_year.objects.get(start_date = fy_start, end_date = fy_end, is_active = True)
 		except financial_year.DoesNotExist:
-			return APIException({"error": "Financial year not found."}, status=status.HTTP_400_BAD_REQUEST)
+			return APIException({"error": "Financial year not found."}, status = status.HTTP_400_BAD_REQUEST)
 		created_slabs = []
-		skipped = []
 
 		for slab_group in slabs:
 			regime_name = slab_group.get('regime_name')
 			try:
-				regime = tax_regimes.objects.get(regime_name=regime_name)
+				regime = tax_regimes.objects.get(regime_name = regime_name)
 			except tax_regimes.DoesNotExist:
-				raise APIException(detail={"statuscode": 404, "status": "error", "message": f"Tax regime '{regime_name}' not found."})
+				raise APIException(detail = {"statuscode": 404, "status": "error", "message": f"Tax regime '{regime_name}' not found."})
 			for slab in slab_group.get('slab_data', []):
-				slab_from = slab.get('slab_from')
-				slab_to = slab.get('slab_to')
-				slab_rate = slab.get('slab_rate')
 
-				if slab_to > Decimal('999999999.99'):
-					return Response({"error": "slab_to value exceeds maximum allowed."}, status=400)
+				slab_data  = {
+					'tax_regime':regime,
+					'slab_from':slab.get('slab_from'),
+					'slab_to':slab.get('slab_to'),
+					'slab_rate':slab.get('slab_rate')
+				}
+				# print("slab_data=====>>>",slab_data)
+				serializer = TaxSlabSerializer(data = slab_data)
+				# print(serializer.is_valid(), "serializer.is_valid")
+				if serializer.is_valid():
+				
+					slab_from = serializer.validated_data['slab_from']
+					slab_to = serializer.validated_data['slab_to']
+					slab_rate = serializer.validated_data['slab_rate']
 
-				exists = tax_slabs.objects.filter(
-						financial_year=fy,
-						tax_regime=regime,
-						slab_from=slab_from,
-						slab_to=slab_to
-					).exists()
+					if slab_to > Decimal('999999999.99'):
+						raise APIException(detail = {"statuscode": 400, "status": "error", "message": "slab_to value exceeds maximum allowed."})
 
-				if exists:
-					skipped.append({
-						'regime': regime_name,
-						'slab_from': slab_from,
-						'slab_to': slab_to
-					})
-					continue
 
-				slab_obj = tax_slabs.objects.create(
-					financial_year=fy,
-					tax_regime=regime,
-					slab_from=slab_from,
-					slab_to=slab_to,
-					slab_rate=slab_rate,
-					is_active=True
-				)
-				created_slabs.append(slab_obj)
-		serializer = TaxSlabSerializer(created_slabs, many=True)
+					exists = tax_slabs.objects.filter(
+							financial_year = fy,
+							tax_regime = regime,
+							slab_from = slab_from,
+							slab_to = slab_to
+						).exists()
+
+					if exists:
+						return Response({
+							"statuscode": status.HTTP_400_BAD_REQUEST,
+							"status": "error",
+							"message": f"Tax slab for regime '{regime_name}' from {slab_from} to {slab_to} already exists."
+						}, status = status.HTTP_400_BAD_REQUEST)
+					
+					print("serializer.validated_data=====>",serializer.validated_data)
+
+					slab_obj = tax_slabs.objects.create(
+						financial_year = fy,
+						tax_regime = regime,
+						slab_from = slab_from,
+						slab_to = slab_to,
+						slab_rate = slab_rate,
+						is_active = True
+					)
+					created_slabs.append(slab_obj)
+				else:
+					return Response({
+						"statuscode": status.HTTP_400_BAD_REQUEST,
+						"status": "error",
+						"message": serializer.errors
+					}, status = status.HTTP_400_BAD_REQUEST)
 		return Response({
+			"statuscode": status.HTTP_201_CREATED,
+			"status": "success",
 			"message": "Tax slabs created successfully.",
-			"created_slabs": serializer.data,
-			"skipped": skipped
-		}, status=status.HTTP_201_CREATED)
+		}, status = status.HTTP_201_CREATED)
 	
 	except financial_year.DoesNotExist:
-		raise APIException(detail={"statuscode": 404, "status": "error", "message": "Active financial year not found."})
+		raise APIException(detail = {"statuscode": 404, "status": "error", "message": "Active financial year not found."})
 	except tax_regimes.DoesNotExist:
-		raise APIException(detail={"statuscode": 404, "status": "error", "message": "Tax regime not found."})
+		raise APIException(detail = {"statuscode": 404, "status": "error", "message": "Tax regime not found."})
 	except Exception as e:
 		return Response({
 			"statuscode": status.HTTP_400_BAD_REQUEST,
 			"status": "error",
 			"message": str(e)
-		}, status=status.HTTP_400_BAD_REQUEST)
+		}, status = status.HTTP_400_BAD_REQUEST)
 	
 
 
@@ -233,6 +251,47 @@ def setup_tax_slabs(request):
 # 		# "message": "Tax calculated successfully."
 # 	}, status=status.HTTP_200_OK)
 
+@api_view(['PATCH'])
+def update_tax_slab(request, pk):
+	try:
+		# Fetch the existing tax slab using the ID from path
+		try:
+			slab = tax_slabs.objects.get(tax_slab_id = pk)
+		except tax_slabs.DoesNotExist:
+			return Response({
+				"statuscode": status.HTTP_404_NOT_FOUND,
+				"status": "error",
+				"message": f"Tax slab with ID {pk} not found."
+			}, status = status.HTTP_404_NOT_FOUND)
+
+		# Validate the input using the updated serializer
+		serializer = TaxSlabUpdateSerializer(data = request.data, partial = True)
+		if serializer.is_valid():
+			# Update the fields if provided
+			for field, value in serializer.validated_data.items():
+				setattr(slab, field, value)
+
+			slab.save()
+
+			return Response({
+				"statuscode": status.HTTP_200_OK,
+				"status": "success",
+				"message": "Tax slab updated successfully."
+			}, status = status.HTTP_200_OK)
+
+		else:
+			return Response({
+				"statuscode": status.HTTP_400_BAD_REQUEST,
+				"status": "error",
+				"message": serializer.errors
+			}, status = status.HTTP_400_BAD_REQUEST)
+
+	except Exception as e:
+		return Response({
+			"statuscode": status.HTTP_400_BAD_REQUEST,
+			"status": "error",
+			"message": str(e)
+		}, status = status.HTTP_400_BAD_REQUEST)
 
 
 
@@ -246,19 +305,19 @@ def calculate_employee_tax_deduction(employee_id, ctc):
 	fy_end = date(current_year + 1, 3, 31)
 
 	try:
-		current_fy = financial_year.objects.get(start_date=fy_start, end_date=fy_end, is_active=True)
+		current_fy = financial_year.objects.get(start_date = fy_start, end_date = fy_end, is_active = True)
 	except financial_year.DoesNotExist:
 		return Response({
 			"statuscode": status.HTTP_400_BAD_REQUEST,
 			"status": "error",
 			"message": "financial year not found"
-		}, status=status.HTTP_400_BAD_REQUEST)
+		}, status = status.HTTP_400_BAD_REQUEST)
 
 	# Get employee's selected tax regime for current FY
 	regime_selection = employee_tax_regimes.objects.get(
-		employee_id=employee_id,
-		financial_year=current_fy,
-		is_active=True
+		employee_id = employee_id,
+		financial_year = current_fy,
+		is_active = True
 	)
 	print("regime_selection",regime_selection)
 	try:
@@ -268,13 +327,13 @@ def calculate_employee_tax_deduction(employee_id, ctc):
 			"statuscode": status.HTTP_400_BAD_REQUEST,
 			"status": "error",
 			"message": "regime not found"
-		}, status=status.HTTP_400_BAD_REQUEST)
+		}, status = status.HTTP_400_BAD_REQUEST)
 	
 	# Get tax slabs for this FY and regime
-	slabs = tax_slabs.objects.filter(
-		financial_year=current_fy,
-		tax_regime=regime,
-		is_active=True
+	slabs  =  tax_slabs.objects.filter(
+		financial_year = current_fy,
+		tax_regime = regime,
+		is_active = True
 	).order_by('slab_from')
 
 	if not slabs.exists():
@@ -282,7 +341,7 @@ def calculate_employee_tax_deduction(employee_id, ctc):
 			"statuscode": status.HTTP_400_BAD_REQUEST,
 			"status": "error",
 			"message": "slab not found"
-		}, status=status.HTTP_400_BAD_REQUEST)
+		}, status = status.HTTP_400_BAD_REQUEST)
 
 	# Apply slabs progressively
 	total_tax = Decimal('0.00')
@@ -306,12 +365,12 @@ def calculate_employee_tax_deduction(employee_id, ctc):
 @api_view(('PATCH',))
 def update_employee_regime(request, pk):
 	try:
-		tax_regime = employee_tax_regimes.objects.get(employee_id=pk)
+		tax_regime = employee_tax_regimes.objects.get(employee_id = pk)
 	except employee_tax_regimes.DoesNotExist:
-		raise APIException(detail={"statuscode": 404, "status": "error", "message": "Employee tax regime not found"})
-	serializer = UpdateTaxregimeSerializer(tax_regime, data=request.data, partial=True)
+		raise APIException(detail = {"statuscode": 404, "status": "error", "message": "Employee tax regime not found"})
+	serializer = UpdateTaxregimeSerializer(tax_regime, data = request.data, partial = True)
 	if serializer.is_valid():
 		employee_tax_regimes.objects.filter(employee_id = pk).update(**serializer.validated_data)
-		return Response({"statuscode": status.HTTP_200_OK, "status": "success", "message": "Employee tax regime updated successfully"}, status=status.HTTP_200_OK)
+		return Response({"statuscode": status.HTTP_200_OK, "status": "success", "message": "Employee tax regime updated successfully"}, status = status.HTTP_200_OK)
 	else:
-		return Response({"statuscode": status.HTTP_400_BAD_REQUEST, "status": "error", "message": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+		return Response({"statuscode": status.HTTP_400_BAD_REQUEST, "status": "error", "message": serializer.errors}, status = status.HTTP_400_BAD_REQUEST)
